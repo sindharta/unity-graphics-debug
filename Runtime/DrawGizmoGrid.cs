961ed0c964563f0ce0a8f18002819ee840cd99cd
Sindharta Tanuwijaya <sindharta@gmail.com>
2020-01-20 19:28:05 +0900

    Untabify

--- Runtime/DrawGizmoGrid.cs
+++ Runtime/DrawGizmoGrid.cs
@@ -5,59 +5,59 @@ namespace Shin.GraphicsDebug {
 
 [ExecuteInEditMode]
 public class DrawGizmoGrid : GameBehaviour {
-	// rename + centre the gameobject upon first time dragging the script into the editor. 
+    // rename + centre the gameobject upon first time dragging the script into the editor. 
 
 #if UNITY_EDITOR
     void Reset () {
-		if (name == "GameObject")
-			name = "~~ GIZMO GRID ~~"; 
+        if (name == "GameObject")
+            name = "~~ GIZMO GRID ~~"; 
  
         m_transform.position = Vector3.zero; 
-	}
+    }
 #endif
 
 //----------------------------------------------------------------------------------------------------------------------
 
-	void OnDrawGizmos () {
-		// orient to the gameobject, so you can rotate the grid independently if desired
-		Gizmos.matrix = m_transform.localToWorldMatrix;
+    void OnDrawGizmos () {
+        // orient to the gameobject, so you can rotate the grid independently if desired
+        Gizmos.matrix = m_transform.localToWorldMatrix;
  
-		// set colours
-		Color dimColor = new Color(m_gizmoLineColor.r, m_gizmoLineColor.g, m_gizmoLineColor.b, 0.25f* m_gizmoLineColor.a); 
-		Color brightColor = Color.Lerp (Color.white, m_gizmoLineColor, 0.75f); 
+        // set colours
+        Color dimColor = new Color(m_gizmoLineColor.r, m_gizmoLineColor.g, m_gizmoLineColor.b, 0.25f* m_gizmoLineColor.a); 
+        Color brightColor = Color.Lerp (Color.white, m_gizmoLineColor, 0.75f); 
  
-		// draw the horizontal lines
-		for (int x = m_minX; x < m_maxX+1; ++x) {
-		    Gizmos.color = ChooseColor(x, m_gizmoMajorLineCounter, m_gizmoLineColor, brightColor, dimColor);
+        // draw the horizontal lines
+        for (int x = m_minX; x < m_maxX+1; ++x) {
+            Gizmos.color = ChooseColor(x, m_gizmoMajorLineCounter, m_gizmoLineColor, brightColor, dimColor);
  
-			Vector3 pos1 = new Vector3(x, m_minY, 0) * m_gridScale;  
-			Vector3 pos2 = new Vector3(x, m_maxY, 0) * m_gridScale;  
+            Vector3 pos1 = new Vector3(x, m_minY, 0) * m_gridScale;  
+            Vector3 pos2 = new Vector3(x, m_maxY, 0) * m_gridScale;  
  
-			// convert to topdown/overhead units if necessary
-			if (m_topDownGrid) {
-				pos1 = new Vector3(pos1.x, 0, pos1.y); 
-				pos2 = new Vector3(pos2.x, 0, pos2.y); 
-			}
+            // convert to topdown/overhead units if necessary
+            if (m_topDownGrid) {
+                pos1 = new Vector3(pos1.x, 0, pos1.y); 
+                pos2 = new Vector3(pos2.x, 0, pos2.y); 
+            }
  
-			Gizmos.DrawLine ((m_gridOffset + pos1), (m_gridOffset + pos2)); 
-		}
+            Gizmos.DrawLine ((m_gridOffset + pos1), (m_gridOffset + pos2)); 
+        }
  
-		// draw the vertical lines
-		for (int y = m_minY; y < m_maxY+1; ++y) {
-		    Gizmos.color = ChooseColor(y, m_gizmoMajorLineCounter, m_gizmoLineColor, brightColor, dimColor);
+        // draw the vertical lines
+        for (int y = m_minY; y < m_maxY+1; ++y) {
+            Gizmos.color = ChooseColor(y, m_gizmoMajorLineCounter, m_gizmoLineColor, brightColor, dimColor);
  
-			Vector3 pos1 = new Vector3(m_minX, y, 0) * m_gridScale;  
-			Vector3 pos2 = new Vector3(m_maxX, y, 0) * m_gridScale;  
+            Vector3 pos1 = new Vector3(m_minX, y, 0) * m_gridScale;  
+            Vector3 pos2 = new Vector3(m_maxX, y, 0) * m_gridScale;  
  
-			// convert to topdown/overhead units if necessary
-			if (m_topDownGrid) {
-				pos1 = new Vector3(pos1.x, 0, pos1.y); 
-				pos2 = new Vector3(pos2.x, 0, pos2.y); 
-			}
+            // convert to topdown/overhead units if necessary
+            if (m_topDownGrid) {
+                pos1 = new Vector3(pos1.x, 0, pos1.y); 
+                pos2 = new Vector3(pos2.x, 0, pos2.y); 
+            }
  
-			Gizmos.DrawLine ((m_gridOffset + pos1), (m_gridOffset + pos2)); 
-		}
-	}
+            Gizmos.DrawLine ((m_gridOffset + pos1), (m_gridOffset + pos2)); 
+        }
+    }
 
 //----------------------------------------------------------------------------------------------------------------------
     static Color ChooseColor(int index, int majorCounter, Color gizmoColor, Color brightColor, Color dimColor) {
